8ee938f1a459d4ad912823af23a685386ab9fa9f
Omar <omarqalaweh@gmail.com>
2024-12-21 13:44:57 +0200

    adding student list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-
 import './App.css'
+import Student from './components/student/student.component';
+
+const COURSES_LIST = ['React', 'HTML', 'CSS'];
+
+
 
 function App() {
 
   return (
     <>
-      <h1>Welcome Omar</h1>
+      <h1 style={{ color: '#a3ff55' }}>Welcome to GSG React/Next Course</h1>
       
-    
+      <Student name="Ahmad Saeed" age={18} isGraduated={false} coursesList={COURSES_LIST} />
+      <Student name="Hiba Jameel" age={20} isGraduated={false} coursesList={COURSES_LIST} />
+      <Student name={"Waleed Fadi"} age={23} isGraduated={true} coursesList={COURSES_LIST} />
+      <Student name="Sarah Waheed" age={19} isGraduated={!false} coursesList={COURSES_LIST} />
+      <Student name="Mohammad Ahmad" age={24 - 2} isGraduated={true} coursesList={COURSES_LIST} />
+
     </>
   )
 }
